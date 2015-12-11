13e4149e93d0ce34c1edab58be04952923a39906
Jim Stewart <jim@stewartstuff.com>
2015-12-10 22:26:36 -0600

    Initial Commit

---
+++ Foolin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Foolin
+//
+//  Created by Jim on 12/10/15.
+//  Copyright © 2015 Jim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
